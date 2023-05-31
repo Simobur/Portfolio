9e1d3bd25622ff37e956dbb829fead1da64a2ec0
Simobur <burekszymon.biuro@gmail.com>
2023-05-31 20:38:29 +0200

    add slider to main project, improve queries

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const btnNav = document.querySelector(".btn-nav");
 const mobileNav = document.querySelector(".section-nav-mobile");
 const btnClose = document.querySelector(".btn-nav-close");
 const linkNav = document.querySelectorAll(".link-nav-mobile");
+const btnLeft = document.querySelector(".btn--left");
+const btnRight = document.querySelector(".btn--right");
+const slides = document.querySelectorAll(".slide");
 
 btnNav.addEventListener("click", function () {
   mobileNav.classList.toggle("hidden");
@@ -11,11 +14,29 @@ btnClose.addEventListener("click", function () {
   mobileNav.classList.toggle("hidden");
 });
 
-console.log(linkNav);
-
 linkNav.forEach((link) =>
   link.addEventListener("click", function () {
     console.log("click");
     mobileNav.classList.add("hidden");
   })
 );
+
+let currSlide = 1;
+
+btnRight.addEventListener("click", function () {
+  currSlide++;
+  if (currSlide === slides.length + 1) currSlide = 1;
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${100 * (i - currSlide + 1)}%)`;
+  });
+  console.log(currSlide);
+});
+
+btnLeft.addEventListener("click", function () {
+  currSlide--;
+  if (currSlide < 1) currSlide = slides.length;
+  slides.forEach(
+    (slide, i) =>
+      (slide.style.transform = `translateX(${100 * (i - currSlide + 1)}%)`)
+  );
+});
